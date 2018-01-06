beccd0d3050bc28ac932a466668c1808f424e4d1
webdevj1 <32886854+webdevj1@users.noreply.github.com>
2018-01-06 11:59:42 -0500

    LINK fix...

--- src/pokePage.js
+++ src/pokePage.js
@@ -3,10 +3,12 @@ import {Link, Switch, Route} from 'react=router-dom';
 
 
 const Pokemon = () => {
-  <Link to='/pokemon/name' />
-  <Link to='/pokemon/number'></Link>
-  <Switch>
-  <Route to='/pokemon/name' />
-  <Route to='/pokemon/number' />
-  </Switch>
+  <div>
+    <Link to='/pokemon/name'>Name</Link>
+    <Link to='/pokemon/number'>Number</Link>
+    <Switch>
+    <Route to='/pokemon/name' />
+    <Route to='/pokemon/number' />
+    </Switch>
+  </div>
 }
