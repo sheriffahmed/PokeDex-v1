fc054371b2cf035f07902496c72863b41a75843c
Cody Hess <cody.hess@gmail.com>
2018-01-06 12:29:08 -0500

    hey i made change

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ const Home = () => (
     <h1 style={{ textAlign: 'Center' }}>PokeDex v1: <i>A casual guide to competitive battles</i></h1>
     <p style={{ textAlign: 'left', width: "20em" }}> Explore the digital Encyclopedia of the Pokemon Universe using the search bar above</p>
     <br />
+    <p>CODY IS THE BEST OF ALL TIME</p>
     <p style={{ textAlign: 'left', width: "20em" }}>To view Pokemon, please navigate to the Pokemon page by Clicking on the "Pokemon" tab, or click <Link to="/pokemon">here</Link>.</p>
   </div>
 );
