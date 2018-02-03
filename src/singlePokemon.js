374094c6901c3a528733b8f6fda4f6b05fe0a7a2
sheriffahmed <sheriff.v.ahmed@gmail.com>
2018-02-03 02:00:25 -0500

    Making Single Page

--- src/singlePokemon.js
+++ src/singlePokemon.js
@@ -1,3 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-const SinglePokemon = ()=>{}
+const SinglePokemon = ({name, id, height, weight, abilities, sprites, stats, types, prev })=>{
+
+return(
+  <div>
+  <h1>{name}</h1>
+  <img src={sprites} alt={`loading ${name}`}  />
+  <Link to={`/${prev}`}>Back</Link>
+
+  </div>
+)
+}
